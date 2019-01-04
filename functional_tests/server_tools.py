8892d7a51def9f71b7abbb59654d7d9530785b3a
Heather Huizenga <cephalopodblue@gmail.com>
2019-01-04 10:45:58 -0800

    fix typo in _get_manage_dot_py

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -3,7 +3,7 @@ from fabric.context_managers import settings, shell_env
 
 
 def _get_manage_dot_py(host):
-    return f'~/sites/{host}/virtualenv/bin/python ~/site/{host}/manage.py'
+    return f'~/sites/{host}/virtualenv/bin/python ~/sites/{host}/manage.py'
 
 
 def _get_server_env_vars(host):
