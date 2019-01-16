434e30bc6d5d25e754f8e0bd21c054e97e026a26
Heather Huizenga <cephalopodblue@gmail.com>
2019-01-16 13:39:46 -0800

    Update pip/setuptools too

--- testing-setup.sh
+++ testing-setup.sh
@@ -5,6 +5,6 @@ then
     python3 -m venv virtualenv
 fi
 
-./virtualenv/bin/pip install --update pip setuptools
+./virtualenv/bin/pip install --upgrade pip setuptools
 ./virtualenv/bin/pip install -r requirements.txt
 ./virtualenv/bin/pip install -r test_requirements.txt
